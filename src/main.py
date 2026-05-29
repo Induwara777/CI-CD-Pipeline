9c2023c34a1f1bb6732bf7e5816f47cdc3e158fa
Induwara777 <induwaradilshan7@gmail.com>
2026-05-29 12:51:16 +0530

    Initial visualization project

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_excel("F:/CI CD Pipeline/data/datafile.xlsx")
+plt.figure(figsize=[10,6])
+plt.bar(df['District'].value_counts().index,df['District'].value_counts().values)
+plt.xlabel("District")
+plt.xticks(rotation = "vertical")
+plt.ylabel('Count of District')
+plt.yticks(range(0,65,5))
+plt.title("District Population")
+plt.savefig("F:/CI CD Pipeline/output/output1.png")
+print("Saved images !!!")
